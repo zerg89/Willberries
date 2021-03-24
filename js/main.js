2c7b1b4d89eed707bf79ec401b59e1d0a6ff14af
zerg89 <andreiglumov1989@gmail.com>
2021-03-25 00:52:02 +0200

    add all files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+

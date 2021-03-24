718fd3cba6674e0f50ffa4a7ec444d8e1043e9e2
zerg89 <andreiglumov1989@gmail.com>
2021-03-25 01:40:00 +0200

    add scroll smooth

--- js/main.js
+++ js/main.js
@@ -24,3 +24,21 @@ const closeModal = () => {
 
 buttonCart.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
+
+
+// scroll smooth
+
+{
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', (event) => {
+			event.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		});
+	}
+}
